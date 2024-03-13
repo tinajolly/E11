33b17955e2d5af7516e044b3a5b71c1457cc04ab
tinajolly <158365656+tinajolly@users.noreply.github.com>
2024-03-13 16:54:29 -0700

    Add files via upload

---
+++ radiation_sensor1.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import datetime
+import time
+#import board
+
+now = time.time()
+start_time = time.time()
+run_time = 20
+
+def my_callback(channel):
+    if GPIO.input(channel) == GPIO.LOW:
+        print('Falling Edge at ' + str(datetime.datetime.now()))
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.IN)
+GPIO.add_event_detect(16, GPIO.FALLING, callback=my_callback)
+
+
+
+while now - start_time < 20 :
+    time.sleep(1)
+    
