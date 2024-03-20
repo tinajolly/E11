a3f84130951d64217c23838c0877131b0bd7c161
ec8ton <elliott.caton@berkeley.edu>
2024-03-20 12:49:33 -0700

    Update rad_sensor_2.py

--- Sensors/rad_sensor_2.py
+++ Sensors/rad_sensor_2.py
@@ -5,16 +5,11 @@ import sys
 import csv
 
 file_name1 = sys.argv[1]
-file_name2 = sys.argv[2]
-run_time = int(sys.argv[3])
+run_time = int(sys.argv[2])
+interval_time = int(sys.arg[3])
 
-meta_data = ["Minute","Counts per Minute"]
+meta_data = ["Time","Counts per Interval"]
 file = open(file_name1,"w",newline='')
-data_writer = csv.writer(file)
-data_writer.writerow(meta_data)
-
-meta_data = ["Counts"]
-file = open(file_name2,"w",newline='')
 data_writer = csv.writer(file)
 data_writer.writerow(meta_data)
 
@@ -24,10 +19,10 @@ t = 0
 
 def my_callback(channel):
     if GPIO.input(channel) == GPIO.FALLING:
-        #print('Count at ' + str(datetime.datetime.now()))
+        print('Count at ' + str(datetime.datetime.now()))
         tcounts = tcounts + 1
-        data1 = [str(datetime.datetime.now())]
-        data_writer.writerow(data1)
+        #data1 = [str(datetime.datetime.now())]
+        #data_writer.writerow(data1)
 
 now = time.time()
 start_time = now
@@ -36,14 +31,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN)
 GPIO.add_event_detect(16, GPIO.FALLING, callback=my_callback)
  
-while  ((now - start_time)) < run_time:
-    time.sleep(60)
+while  (now - start_time) < run_time:
+    time.sleep(interval_time)
     now = time.time()
     t = t + 1
     mcounts = tcounts - ocounts
     ocounts = tcounts
-    print("Counts per Mintue =", mcounts)
-    data2 = [t,mcounts]
+    print("Counts per",interval_time,"seconds =", mcounts)
+    data2 = [now,mcounts]
     data_writer.writerow(data2)
 
 GPIO.cleanup()
