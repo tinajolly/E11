7f4b837811706215ecd4fd6c213bc778e65a382f
ec8ton <elliott.caton@berkeley.edu>
2024-03-20 14:46:11 -0700

    Create rad_sensor_3.py

---
+++ Sensors/rad_sensor_3.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import datetime
+import time
+#import board
+
+now = time.time()
+start_time = time.time()
+run_time = 20
+
+def my_callback(channel):
+    print('Count at ' + str(datetime.datetime.now()))
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.IN)
+GPIO.add_event_detect(16, GPIO.FALLING, callback=my_callback)
+
+
+
+while (now - start_time) < 20 :
+    time.sleep(1)
+    
